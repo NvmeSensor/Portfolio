a4bad7088b1c2635e028f8c94a9cdebfbd47af2e
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-05-11 13:13:19 +0000

    portfolio

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import BackgroundAnimation from "@/components/BackgroundAnimation";
+const Home = ()=>{
+  return(
+    <section className="w-full flex flex-col justify-center items-center">
+      <div className="hero">
+      <BackgroundAnimation />
+        <div className="hero-content">
+
+          </div>
+          </div>
+    </section>
+  )
+}
+export default Home;
