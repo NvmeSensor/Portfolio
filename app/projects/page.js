8b866456eab2547814d5ccc7a1a79a12616b23e5
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-05-31 12:11:17 +0000

    portfolio

---
+++ app/projects/page.js
@@ -0,0 +1,11 @@
+import Projects from "@/components/projects/Projects";
+
+const ProjectsHome = () => {
+    return (
+        <section>
+            <Projects />
+        </section>
+    )
+}
+
+export default ProjectsHome;
