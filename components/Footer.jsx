f2b304c0e9c3e751686866a3f82e3a0105e991e5
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-11 12:08:18 +0000

    portfolio

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='flex justify-center items-center w-full card'>
-            <div className='flex flex-col justify-center items-center gap-1 lg:w-[80vw] py-5 w-full'>
+            <div className='flex flex-col justify-center items-center lg:w-[80vw] py-2 w-full'>
                 <h1 className='text-slate-300 font-medium text-center'>Developed By Priyanshu</h1>
                 <h2 className='text-slate-300 font-medium text-center'>Developed with Simplicity in Mind</h2>
             </div>
