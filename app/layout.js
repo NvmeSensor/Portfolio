524b4892d0f0295f0b8fb5d7f88e57b9b38cd6ed
Priyanshu Chahar <165533860+Priyannxhuu@users.noreply.github.com>
2024-06-11 17:57:43 +0530

    portfolio

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 import "./style.css"
 import Footer from '../components/Footer'
@@ -16,16 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords.join(', ')} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Head>
+        <meta name="keywords" content={metadata.keywords} />
+      </head>
       <body className={inter.className}>
         {children}
         <Footer />
