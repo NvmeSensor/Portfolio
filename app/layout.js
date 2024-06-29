f92a17d6ebef649cd6fe76152ee533f06b2f26bb
NvmeSensor <priyannxhuu@gmail.com>
2024-06-30 00:00:24 +0530

    portfolio

--- app/layout.js
+++ app/layout.js
@@ -6,21 +6,30 @@ import Navbar from "@/components/Navbar";
 
 const jetmono = JetBrains_Mono({ subsets: ["latin"] });
 
-export const metadata = {
+const metadata = {
   title: "Priyanshu Chahar | Portfolio",
-  description: "Priyanshu Chahar is a skilled MERN stack developer with expertise in MERN stack technologies. Get in touch for web development projects.",
-  keywords: ["Priyanshu Chahar", "Full Stack Developer", "MERN Stack", "Web Development"],
-  image: "preview.jpg",
+  description: "Priyanshu Chahar is a skilled MERN stack developer with expertise in MongoDB, Express.js, React.js, and Node.js. Contact Priyanshu for professional web development services and projects.",
+  keywords: ["Priyanshu Chahar", "Priyannxhuu", "NvmeSensor", "Full Stack Developer", "MERN Stack", "MongoDB", "Express.js", "React.js", "Node.js", "Web Development"],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <title>{metadata.title}</title>
+        <title>Priyanshu Chahar | Portfolio</title>
+        <meta name="title" content="Priyanshu Chahar | Portfolio" />
         <meta name="description" content={metadata.description} />
-        <meta name="keywords" content={metadata.keywords} />
+        <meta name="keywords" content={metadata.keywords}></meta>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://priyanshuchahar.vercel.app" />
+        <meta property="og:title" content="Priyanshu Chahar | Portfolio" />
+        <meta property="og:description" content={metadata.description}/>
+        <meta property="og:image" content="https://priyanshuchahar.vercel.app/profileimage.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://priyanshuchahar.vercel.app" />
+        <meta property="twitter:title" content="Priyanshu Chahar | Portfolio" />
+        <meta property="twitter:description" content={metadata.description}/>
+        <meta property="twitter:image" content="https://priyanshuchahar.vercel.app/profileimage.png" />
       </head>
       <body className={jetmono.className}>
         {/* <Navbar /> */}
--- components/home/Message.jsx
+++ components/home/Message.jsx
@@ -37,19 +37,19 @@ const Message = () => {
         <section className='flex flex-col justify-center items-center w-full px-5 mb-14'>
             <div className='flex flex-col justify-center items-center gap-3 lg:w-[75vw] w-full'>
                 <div className='flex justify-center items-center gap-4 self-start'>
-                    <h1 className='text-3xl font-semibold self-start text-slate-300 flex gap-2 items-center'>Drop a Message</h1>
+                    <h1 className='text-3xl font-bold self-start text-slate-300 flex gap-2 items-center'>Drop a Message</h1>
                 </div>
                 <form onSubmit={onSubmit} className='p-5 rounded-lg flex flex-col justify-center items-center gap-2 w-full border-b-2 border-slate-400 card'>
-                    <label htmlFor="name" className=' text-slate-100 font-medium self-start text-xl'>Name</label>
-                    <input type="text" id='name' name="name" placeholder='Your Name' className='text-slate-200 font-medium px-3 py-2 rounded-lg w-full outline-none inputt' required />
-                    <label htmlFor="email" className=' text-slate-100 font-medium self-start text-xl'>Email</label>
-                    <input type="email" id='email' name="email" placeholder='Your Email' className='text-slate-200 font-medium px-3 py-2 rounded-lg w-full outline-none inputt' required />
-                    <label htmlFor="message" className=' text-slate-100 font-medium self-start text-xl'>Message</label>
-                    <textarea id='message' name="message" placeholder='Drop Your Message' className='text-slate-200 font-medium px-3 py-2 rounded-lg w-full outline-none inputt' required></textarea>
-                    <button type="submit" className='px-3 py-2 font-semibold bg-slate-200 hover:bg-slate-300  transition ease-in-out rounded-lg mt-2 self-start'>Submit</button>
+                    <label htmlFor="name" className='text-[18px] text-slate-100 font-semibold self-start text-xl'>Name</label>
+                    <input type="text" id='name' name="name" placeholder='Your Name' className='text-slate-200 font-semibold px-3 py-2 rounded-lg w-full outline-none inputt' required />
+                    <label htmlFor="email" className='text-[18px] text-slate-100 font-semibold self-start text-xl'>Email</label>
+                    <input type="email" id='email' name="email" placeholder='Your Email' className='text-slate-200 font-semibold px-3 py-2 rounded-lg w-full outline-none inputt' required />
+                    <label htmlFor="message" className='text-[18px] text-slate-100 font-semibold self-start text-xl'>Message</label>
+                    <textarea id='message' name="message" placeholder='Drop Your Message' className='text-slate-200 font-semibold px-3 py-2 rounded-lg w-full outline-none inputt' required></textarea>
+                    <button type="submit" className='px-3 py-2 font-semibold bg-slate-200 hover:bg-slate-300 text-[18px] transition ease-in-out rounded-lg mt-2 self-start'>Submit</button>
                 </form>
-                {error && <span className="text-slate-300 font-medium">{error}</span>}
-                {result && <span className="text-slate-300 font-medium">{result}</span>}
+                {error && <span className="text-[18px] text-slate-300 font-semibold">{error}</span>}
+                {result && <span className="text-[18px] text-slate-300 font-semibold">{result}</span>}
             </div>
         </section>
     )
