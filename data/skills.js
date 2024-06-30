ef221147749b80ac18b1d917d75f3b2829c7ba8c
NvmeSensor <priyannxhuu@gmail.com>
2024-06-30 08:07:37 +0530

    portfolio

---
+++ data/skills.js
@@ -0,0 +1,126 @@
+import { SiHtml5, SiCss3, SiJavascript, SiTypescript } from "react-icons/si";
+import {
+    RiTailwindCssFill,
+    RiBootstrapFill,
+    RiReactjsFill,
+    RiNextjsFill,
+} from "react-icons/ri";
+import { SiMysql, SiMongodb, SiExpress } from "react-icons/si";
+import { FaGitAlt, FaGithub, FaLanguage } from "react-icons/fa";
+import { FaWindows, FaLinux, FaUbuntu } from "react-icons/fa6";
+import { FaPython, FaJava } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { IoCodeSlashSharp } from "react-icons/io5";
+import { FaRust } from "react-icons/fa";
+import { TbBrandCpp } from "react-icons/tb";
+import { SiCsharp } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
+import { SiMongoose } from "react-icons/si";
+
+const skills = [
+    {
+        name: "HTML",
+        icon: <SiHtml5 />,
+    },
+    {
+        name: "CSS",
+        icon: <SiCss3 />,
+    },
+    {
+        name: "Javascript",
+        icon: <SiJavascript />,
+    },
+    {
+        name: "Typescript",
+        icon: <SiTypescript />,
+    },
+    {
+        name: "Tailwind",
+        icon: <RiTailwindCssFill />,
+    },
+    {
+        name: "Bootstrap",
+        icon: <RiBootstrapFill />,
+    },
+    {
+        name: "React JS",
+        icon: <RiReactjsFill />,
+    },
+    {
+        name: "Next JS",
+        icon: <RiNextjsFill />,
+    },
+    {
+        name: "NodeJS",
+        icon: <FaNodeJs />,
+    },
+    {
+        name: "ExpressJS",
+        icon: <SiExpress />,
+    },
+    {
+        name: "MongoDB",
+        icon: <SiMongodb />,
+    },
+    {
+        name: "Mongoose",
+        icon: <SiMongoose />,
+    },
+    {
+        name: "MySql",
+        icon: <SiMysql />,
+    },
+
+    // {
+    //   name: "Ubuntu",
+    //   icon: <FaUbuntu />,
+    // },
+    {
+        name: "Python",
+        icon: <FaPython />,
+    },
+    {
+        name: "Basic C#",
+        icon: "",
+    },
+    {
+        name: "Basic Java",
+        icon: <FaJava />,
+    },
+    {
+        name: "Basic C++",
+        icon: "",
+    },
+    {
+        name: "Basic Rust",
+        icon: <FaRust />,
+    },
+];
+
+const tools = [
+    {
+        name: "Git",
+        icon: <FaGitAlt />,
+    },
+    {
+        name: "Github",
+        icon: <FaGithub />,
+    },
+    {
+        name: "VsCode",
+        icon: <VscVscode />,
+    }
+]
+
+const os = [
+    {
+        name: "Windows",
+        icon: <FaWindows />,
+    },
+    {
+        name: "Linux",
+        icon: <FaLinux />,
+    }
+]
+
+export { skills, tools, os };
